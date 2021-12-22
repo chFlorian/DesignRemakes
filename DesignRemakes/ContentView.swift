1be691160f0cddd39b8bea4558cacb6b69349e4d
Florian Schweizer <post.an.flo@gmail.com>
2021-12-22 09:31:11 +0100

    Initial Commit

---
+++ DesignRemakes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignRemakes
+//
+//  Created by Florian Schweizer on 22.12.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignRemakes/DesignRemakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignRemakesApp.swift
+//  DesignRemakes
+//
+//  Created by Florian Schweizer on 22.12.21.
+//
+
+import SwiftUI
+
+@main
+struct DesignRemakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
